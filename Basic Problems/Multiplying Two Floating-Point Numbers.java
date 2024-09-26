590f9db64f3c33f5ea67f345d2e07da4c6441a4e
Ramasai2201 <ramsaithrinad@gmail.com>
2024-09-26 22:26:02 +0530

    Update Multiplying Two Floating-Point Numbers.java

--- Basic Problems/Multiplying Two Floating-Point Numbers.java
+++ Basic Problems/Multiplying Two Floating-Point Numbers.java
@@ -1,1 +1,11 @@
-
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        float f1= sc.nextFloat();
+        float f2= sc.nextFloat();
+        
+        float Fm= (f1)*(f2);
+    System.out.println("The Multiplication of Two Numbers is = " + Fm);
+    }
+}
